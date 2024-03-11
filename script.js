9b4d73203d6d05b64444f5175f8e56b8d4428908
Joe <csp.x520@gmail.com>
2024-03-11 19:34:28 -0400

    Fixed logic conditional check for winner conditions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
     const columns = 3;
     const board = [];
 
-    for (i=0; i<rows; i++){
+    for (let i=0; i<rows; i++){
         board[i]=[];
         for (j=0; j<columns; j++){
             board[i][j]=[" "];
@@ -15,13 +15,15 @@
     }
 
     const updateBoard = () => {
+        let input = prompt("Enter X or O");
         let rowIndex = prompt("Enter row index (0-2): ");
         let colIndex = prompt("Enter column index (0-2): ");
-        let input = prompt("Enter X or O");
 
         if(rowIndex >= 0 && rowIndex < rows && colIndex >= 0 && colIndex < columns && input == "X" || input =="O"){
             if(board[rowIndex][colIndex] == "X" || board[rowIndex][colIndex] == "O"){
                 console.log("Invalid input. Please X or O in a different index position.");
+                displayBoard();
+                checkWinner();
             }
             else{
                 board[rowIndex][colIndex]=input;
@@ -31,29 +33,31 @@
         }
         else{
             console.log("Invalid input. Please re-enter correct row/column index or X or O");
+            updateBoard();
         }
     }
     
     const checkWinner = () => {
         //Checks winner via rows
-        for(i=0; i<rows; i++){
+        for(let i=0; i<rows; i++){
             let j=0;
-            if(board[i][j] == board[i][j+1] == board[i][j+2]){
+            if(board[i][j] === board[i][j+1] && board[i][j+2] === board[i][j] && (board[i][j]=== "X" || board[i][j] === "O")){
                 console.log("You Win");
             }
         }
         //Checks winner via columns
-        for(j=0; j<columns; j++){
+        for(let j=0; j<columns; j++){
             let i=0;
-            if(board[i][j] == board[i+1][j] == board[i+2][j]){
+            if(board[i][j] === board[i+1][j] && board[i+2][j] === board[i][j] && (board[i][j]=== "X" || board[i][j] === "O")){
                 console.log("You Win");
             }
         }
         //Checks winner via Diagonal Cross
         let i = 0;
-        if(board[i][i] == board[i+1][i+1] == board[i+2][i+2]){
+        if((board[i][i] === board[i+1][i+1] && board[i+2][i+2] === board[i][i] && (board[i][i]=== "X" || board[i][i] === "O")) ||(board[i][i+2] === board[i+1][i+1] && board[i+2][i] === board[i][i+2] && (board[i][i+2]=== "X" || board[i][i+2] === "O"))){
             console.log("You Win");
         }
+        updateBoard();
     }
     displayBoard();
     updateBoard();
